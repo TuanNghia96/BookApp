04d276be962a8f7caf4067f3e926b9d9155314d8
TuanNghia96 <nghia.nbt143167@sis.hust.edu.vn>
2018-10-17 10:22:49 +0700

    add search in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,34 +4,49 @@
 		<div class="navbar-header">
 			<a class="navbar-brand" href="{{route('home')}}">HUST phone shop</a>
 		</div>
+				<div id="bill" class="nav navbar-nav navbar-right">
+			<a href=""><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
+		</div>
+		<ul class="nav navbar-nav navbar-right">
 
-<div id="search" class="col-md-offset-4">
+			<li>
+				<a href="{{route('getBill')}}" style="padding-right: 0px;">({{Session::get('number')}})</a>
+			</li>
+			<li>
+				<a href="{{route('getBill')}}" style="margin-right: 30px;"><span class="glyphicon glyphicon-shopping-cart" style="font-size:25px;" aria-hidden="true"></span></a>
+			</li>
+			
+			</ul>
+		<div id="search" class="col-md-offset-5">
 
-<form class="navbar-form navbar-collapse" action="{{route('postSearch')}}" method="post">
-	 	@csrf
-    <div class="row">
-            <div class="input-group col-md-6">
-                <input type="text" class="form-control" name="name" id="inputPassword2" placeholder="tìm điện thoại">
-                <span class="input-group-btn">
-                    <button type="submit" class="btn btn-default">Tìm kiếm.</button>
-                </span>
-            </div>
+			<form class="navbar-form navbar-collapse" action="{{route('postSearch')}}" method="post">
+				 	@csrf
+			    <div class="row">
+			            <div class="input-group col-md-5">
+			                <input type="text" class="form-control" name="name" id="inputPassword2" placeholder="tìm điện thoại">
+			                <span class="input-group-btn">
+			                    <button type="submit" class="btn btn-default">Tìm kiếm.</button>
+			                </span>
+			            </div>
 
-    </div>
-</form>
+			    </div>
+			</form>
 
-</div>
-
-
+		</div>
 
 
-
-{{-- 		<form class="form-inline" action="{{route('postSearch')}}" method="post">
-	
-<div class="form-group">
-<input type="text" class="form-control" name="name" id="inputPassword2" placeholder="tìm điện thoại">
-</div>
-</form> --}}
-
+{{-- 		      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul> --}}
 	</nav>
 </div>
