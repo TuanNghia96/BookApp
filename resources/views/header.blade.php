b42050392ee47eb1c1eb47a3302aa5884cd73e41
TuanNghia96 <nghia.nbt143167@sis.hust.edu.vn>
2018-10-17 11:32:10 +0700

    dropdown in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,52 +1,53 @@
 {{-- header --}}
-<div id ="header" style="">
-	<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+
+<nav class="navbar navbar-inverse navbar-static">
+	<div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+			</button>
 			<a class="navbar-brand" href="{{route('home')}}">HUST phone shop</a>
 		</div>
-				<div id="bill" class="nav navbar-nav navbar-right">
-			<a href=""><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
-		</div>
-		<ul class="nav navbar-nav navbar-right">
 
-			<li>
-				<a href="{{route('getBill')}}" style="padding-right: 0px;">({{Session::get('number')}})</a>
-			</li>
-			<li>
-				<a href="{{route('getBill')}}" style="margin-right: 30px;"><span class="glyphicon glyphicon-shopping-cart" style="font-size:25px;" aria-hidden="true"></span></a>
-			</li>
-			
+	<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class=""><a href="#">Link <span class="sr-only">(current)</span></a></li>
+				<li><a href="#">Tìm kiếm sản phẩm</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Liên hệ<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Liên kết khách hàng.</a></li>
+						<li><a href="#">Giới thiệu website</a></li>
+						<li><a href="#">Thông tin liên hệ</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="#">Thông tin đến người quản trị</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="#">Dăng nhập( quản trị viên)</a></li>
+					</ul>
+				</li>
 			</ul>
-		<div id="search" class="col-md-offset-5">
-
-			<form class="navbar-form navbar-collapse" action="{{route('postSearch')}}" method="post">
-				 	@csrf
-			    <div class="row">
-			            <div class="input-group col-md-5">
-			                <input type="text" class="form-control" name="name" id="inputPassword2" placeholder="tìm điện thoại">
-			                <span class="input-group-btn">
-			                    <button type="submit" class="btn btn-default">Tìm kiếm.</button>
-			                </span>
-			            </div>
-
-			    </div>
+		{{-- search name phone	 --}}
+			<form class="navbar-form navbar-left" action="{{route('postSearch')}}" method="post">
+				@csrf
+				<div class="input-group">
+					<input type="text" class="form-control" name="name" id="inputPassword2" placeholder="Tên điện thoại">
+					<span class="input-group-btn">
+						<button type="submit" class="btn btn-default">Tìm kiếm.</button>
+					</span>
+				</div>
 			</form>
+		{{-- bill status --}}
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="{{route('getBill')}}" style="padding-right: 0px;">({{Session::get('number')}})</a>
+				</li>
+				<li>
+					<a href="{{route('getBill')}}" style="margin-right: 30px;"><span class="glyphicon glyphicon-shopping-cart" style="font-size:25px;" aria-hidden="true"></span></a>
+				</li>
 
-		</div>
-
-
-{{-- 		      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul> --}}
-	</nav>
-</div>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
